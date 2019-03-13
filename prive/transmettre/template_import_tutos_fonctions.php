ff05ff6350908eb67ef43bf9c6e1105583f29067
Rainer <rainer@websimple.be>
2019-03-13 10:12:40 +0100

    ajouter le statut

--- prive/transmettre/template_import_tutos_fonctions.php
+++ prive/transmettre/template_import_tutos_fonctions.php
@@ -3,7 +3,7 @@
 function tuto_champ_editables() {
 	$table_tuto = lister_tables_objets_sql('spip_tutos');
 	$champs = $table_tuto['champs_editables'];
-	$champs[] = 'lang';
+	$champs = array_merge($champs, ['lang', 'statut']);
 	$champs = array_diff($champs,['id_secteur']);
 	return implode(',',$champs);
 }
