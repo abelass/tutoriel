a6456168244bc2cf37ab39e4f4c92ce9a9517f55
Rainer Müller <rainer@websimple.be>
2019-11-10 18:50:44 +0100

    Traductions en anglais et néerlandais

---
+++ lang/tuto_en.php
@@ -0,0 +1,52 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_tuto' => 'Add this tutorial',
+
+	// C
+	'champ_commentaires_label' => 'Comments',
+	'champ_csv_label' => 'Csv file:',
+	'champ_url_label' => 'URL',
+	'confirmer_supprimer_tuto' => 'Do you confirm the removal of this tutorial?',
+
+	// I
+	'icone_creer_tuto' => 'Create a tutorial',
+	'icone_modifier_tuto' => 'Edit this tutorial',
+	'info_1_tuto' => 'One tutorial',
+	'info_aucun_tuto' => 'No tutorial',
+	'info_nb_tutos' => '@nb@ tutorials',
+	'info_tutos_auteur' => 'The tutorials of this author',
+
+	// M
+	'message_erreur_fichier_manquant' => 'Select a file first',
+	'message_tutos_importe' => '@nombre@ tutorials were imported!',
+
+	// R
+	'retirer_lien_tuto' => 'Remove this tutorial',
+	'retirer_tous_liens_tutos' => 'Remove all tutorials',
+
+	// S
+	'supprimer_tuto' => 'Delete this tutorial',
+
+	// T
+	'texte_ajouter_tuto' => 'Add a tutorial',
+	'texte_changer_statut_tuto' => 'This tutorial is:',
+	'texte_creer_associer_tuto' => 'Create and associate a tutorial',
+	'texte_definir_comme_traduction_tuto' => 'This tutorial is a translation of the tutorial number:',
+	'titre_importer_tutos' => 'Importing tutorials',
+	'titre_langue_tuto' => 'Language of this tutorial',
+	'titre_logo_tuto' => 'Logo of this tutorial',
+	'titre_objets_lies_tuto' => 'Related to this tutorial',
+	'titre_telecharger_template' => 'Download the template',
+	'titre_tuto' => 'Tutorial',
+	'titre_tutos' => 'Tutorials',
+	'titre_tutos_rubrique' => 'Tutorials van de rubriek',
+);
---
+++ lang/tuto_nl.php
@@ -0,0 +1,52 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'ajouter_lien_tuto' => 'Voeg deze tutorial toe',
+
+	// C
+	'champ_commentaires_label' => 'Opmerkingen',
+	'champ_csv_label' => 'Csv file:',
+	'champ_url_label' => 'URL',
+	'confirmer_supprimer_tuto' => 'Bevestig je het verwijderen van deze tutorial?',
+
+	// I
+	'icone_creer_tuto' => 'Maak een tutorial',
+	'icone_modifier_tuto' => 'Bewerk deze zelfstudie',
+	'info_1_tuto' => 'Een tutorial',
+	'info_aucun_tuto' => 'Geen tutorial',
+	'info_nb_tutos' => '@nb@ tutorials',
+	'info_tutos_auteur' => 'De tutorials van deze auteur',
+
+	// M
+	'message_erreur_fichier_manquant' => 'Selecteer eerst een bestand',
+	'message_tutos_importe' => '@nombre@ tutorials werden geïmporteerd!',
+
+	// R
+	'retirer_lien_tuto' => 'Verwijder deze zelfstudie',
+	'retirer_tous_liens_tutos' => 'Verwijder alle tutorials',
+
+	// S
+	'supprimer_tuto' => 'Verwijder deze zelfstudie',
+
+	// T
+	'texte_ajouter_tuto' => 'Voeg een tutorial toe',
+	'texte_changer_statut_tuto' => 'Deze tutorial is:',
+	'texte_creer_associer_tuto' => 'Maak en koppel een tutorial',
+	'texte_definir_comme_traduction_tuto' => 'Deze tutorial is een vertaling van het tutorial nummer:',
+	'titre_importer_tutos' => 'Zelfstudies tutorial',
+	'titre_langue_tuto' => 'Taal van deze tutorial',
+	'titre_logo_tuto' => 'Logo van deze tutorial',
+	'titre_objets_lies_tuto' => 'Gerelateerd aan deze tutorial',
+	'titre_telecharger_template' => 'Download de sjabloon',
+	'titre_tuto' => 'Tutorial',
+	'titre_tutos' => 'Tutorials',
+	'titre_tutos_rubrique' => 'Tutoriaux de la rubrique',
+);
