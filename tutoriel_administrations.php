3c322c465b95d8ebe07d3d8376a6182d4d2940e3
Rainer Müller <rainer@websimple.be>
2020-02-25 13:17:23 +0100

    nettoyage

--- tutoriel_administrations.php
+++ tutoriel_administrations.php
@@ -33,24 +33,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 function tutoriel_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 	$maj['1.0.3.2']  = array(array('sql_alter','TABLE spip_tutos DROP COLUMN resultat'));
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	#
-	# $maj['create'] = array(array('creer_base'));
-	#
-	# include_spip('inc/config')
-	# $maj['create'] = array(
-	#	array('maj_tables', array('spip_xx', 'spip_xx_liens')),
-	#	array('ecrire_config', 'tutoriel', array('exemple' => "Texte de l'exemple"))
-	#);
-	#
-	# $maj['1.1.0']  = array(array('sql_alter','TABLE spip_xx RENAME TO spip_yy'));
-	# $maj['1.2.0']  = array(array('sql_alter','TABLE spip_xx DROP COLUMN id_auteur'));
-	# $maj['1.3.0']  = array(
-	#	array('sql_alter','TABLE spip_xx CHANGE numero numero int(11) default 0 NOT NULL'),
-	#	array('sql_alter','TABLE spip_xx CHANGE texte petit_texte mediumtext NOT NULL default \'\''),
-	# );
-	# ...
+
 
 	$maj['create'] = array(array('maj_tables', array('spip_tutos')));
 
@@ -72,10 +55,6 @@ function tutoriel_upgrade($nom_meta_base_version, $version_cible) {
  * @return void
 **/
 function tutoriel_vider_tables($nom_meta_base_version) {
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	# sql_drop_table('spip_xx');
-	# sql_drop_table('spip_xx_liens');
 
 	sql_drop_table('spip_tutos');
 
